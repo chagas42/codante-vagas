4f92b4930015f3e2056b90fec1ca6d5f54a71f1b
Juniors Chagas <jjuniorchagas15@gmail.com>
2025-03-10 02:55:59 -0300

    feat(not-found): enhance Not Found page layout and styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,26 @@
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Nada encontrado aqui...</h2>
-      <img src="/boat.png" alt="ilustracão de um barco" />
-      <p>Não foi possível encontrar o recurso solicitado</p>
-      <a href="/">Voltar para Home</a>
-    </div>
+    <main className="max-w-5xl mx-auto py-20 text-center">
+      <h2 className="text-4xl font-display font-black ">
+        Nada encontrado aqui...
+      </h2>
+      <img
+        className="mx-auto w-sm"
+        src="/boat.png"
+        alt="ilustração de um barco"
+      />
+      <p className="text-3xl font-extralight text-gray-400">
+        Não foi possível encontrar a página solicitada.
+      </p>
+      <a
+        className={cn('mt-10 block', buttonVariants({ variant: 'default' }))}
+        href="/"
+      >
+        Voltar à Home
+      </a>
+    </main>
   );
 }
