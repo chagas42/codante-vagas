08212a7f58122ee5585b48eac7cbf19537fa54e4
Juniors Chagas <jjuniorchagas15@gmail.com>
2025-03-10 21:34:14 -0300

    feat(vagas): fetch and display job listings dynamically using JobItem component

--- src/app/vagas/page.tsx
+++ src/app/vagas/page.tsx
@@ -1,14 +1,27 @@
-import JobItem from '@/components/cards/job-item';
+import JobItem, { Job } from '@/components/cards/job-item';
 
-export default function Vagas() {
+async function fetchJobs() {
+  const res = await fetch('https://apis.codante.io/api/job-board/jobs');
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch jobs');
+  }
+
+  return res.json();
+}
+
+export default async function Vagas() {
+  const result = await fetchJobs();
+
+  const jobs: Job[] = result.data;
+
   return (
     <main className="py-10">
       <h2 className="font-display mb-12 text-2xl font-bold">Todas as Vagas</h2>
       <div className="space-y-8">
-        <JobItem />
-        <JobItem />
-        <JobItem />
-        <JobItem />
+        {jobs.map((job) => (
+          <JobItem job={job} key={job.id} />
+        ))}
       </div>
     </main>
   );
